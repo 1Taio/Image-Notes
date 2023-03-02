c8b3c096fa1afc32a7ee4b61605cbdc70261604b
1Taio <agarciacontreras@student.rtc.edu>
2023-03-01 19:19:46 -0800

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace Image_Notes
 {
@@ -23,6 +24,46 @@ namespace Image_Notes
         public MainWindow()
         {
             InitializeComponent();
+
+            
+        }
+
+
+        public void DisplayImageTest()
+        {
+            //OpenFileDialog dlg = new OpenFileDialog();
+            //dlg.InitialDirectory = "c:\\";
+            //dlg.Filter = "Image files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+            //dlg.RestoreDirectory = true;
+
+            //if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //{
+            //    string selectedFileName = dlg.FileName;
+            //    FileNameLabel.Content = selectedFileName;
+
+            //    // In order to get a bitmap, "image?", start here
+            //    BitmapImage bitmap = new BitmapImage();
+            //    bitmap.BeginInit();
+            //    //we put image location in uri
+            //    bitmap.UriSource = new Uri(selectedFileName);
+            //    bitmap.EndInit();
+
+            //    // Probably display our image to our image control
+            //    ImageViewer1.Source = bitmap;
+            //}
+        }
+
+        private void btnDisplayImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                imageDisplay.Source = new BitmapImage(new Uri(op.FileName));
+            }
         }
     }
 }
